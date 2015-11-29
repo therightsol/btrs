42cc3cf0af619809d92c22d37bda0b332ca17592
Ali Shan <alishan@therightsol.com>
2015-11-30 00:47:45 +0500

    some comments are added

--- application/controllers/about.php
+++ application/controllers/about.php
@@ -2,12 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class About extends CI_Controller{
-    
+    /*
+     * Default function.
+     * My About controller.
+     */
     public function index(){
         $data['page'] = 'about';
         $this->load->view('about' , $data);
     }
-    
 }
 
 ?>
