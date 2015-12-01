8d22cf78c0e5466da039dbd86a7cff8d2d1b37c6
seemisam <asksamina@hotmail.com>
2015-12-01 23:14:46 +0500

    Revert "some changes"
    This reverts commit 2a2c35fe4d8280d6a029184222081a54d484ae69.

--- application/controllers/contact.php
+++ application/controllers/contact.php
@@ -3,10 +3,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Contact extends CI_Controller{
-     /*
-     * Default function.
-     * .
-     */
+    
     public function index(){
         $data['page'] = 'contact';
         $this->load->view('contact' , $data);
