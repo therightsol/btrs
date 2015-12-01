a26c53fb17d27387f2672bc91fa83eb1b59f868b
seemisam <asksamina@hotmail.com>
2015-12-02 04:52:20 +0500

    member controller (done)

---
+++ application/controllers/member.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Member extends CI_Controller{
+    public function index (){
+        $data['page']='';
+        $this->load->view('member',$data);
+    }
+    }
+    
+    
