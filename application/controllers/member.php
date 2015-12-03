de5c6d4697e0b605611f4796c9dd8f9867000551
seemisam <asksamina@hotmail.com>
2015-12-04 02:15:32 +0500

    Done. closes #11

--- application/controllers/member.php
+++ application/controllers/member.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Member extends CI_Controller{
     public function index (){
        
-        $data['page']='';
+        $data['page']='member';
         
         $this->load->view('member',$data);
     }
